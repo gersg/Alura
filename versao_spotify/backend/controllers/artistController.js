3baa64a3d575372e406707d6ed1935896de454a0
Gerson Espindola <gersgdev@gmail.com>
2025-02-14 00:42:37 -0300

    feature funcao para obter um artista pelo id

--- versao_spotify/backend/controllers/artistController.js
+++ versao_spotify/backend/controllers/artistController.js
@@ -5,3 +5,13 @@ const { artists } = require("../db.js");
 const getAllArtists = (req, res) => {
   res.json(artists);
 };
+
+// Função para obter um artista pelo id
+const getArtistById = (req, res) => {
+    const artist = artists.find(a => a.id === parseInt(req.params.id));
+    if (artist) {
+      res.json(artist);
+    } else {
+      res.status(404).json({ message: "Artista não encontrado" });
+    }
+  };
